2459f54facc1b6c435ec9aa91b686a498e1145f5
João Pedro Mamede <joaopedrodias@live.com>
2025-03-21 19:20:49 -0300

    updates

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+
+  photoCover:string = "https://wallpapers.com/images/hd/iron-man-pictures-cfycee82exgsj73t.jpg"
+  cardTitle:string ="NOVO HOMEM DE FERRO EM 3D"
+  cardDescription:string= "Marvel Studios anunciam novo filme do Homem de Ferro em 3D"
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+      
+  }
 
 }
