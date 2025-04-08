abcb7eb2487598f53eff7ec3e5988a8d76843cf4
jpmamededs <joaopedrodias@live.com>
2025-04-08 20:39:25 -0300

    ajustando pra entrevista

--- src/app/components/data/dataFake.ts
+++ src/app/components/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
     {
-        "id":"1",
-        "photo":"aaa",
-        "title": "saiu novo filme",
-        "description":"ola"
+        "Id": "1",
+        "photo": "https://img.elo7.com.br/product/zoom/1017B7A/painel-homem-de-ferro-g-frete-gratis-marvel.jpg",
+        "title": "Novo filme do Homem de Ferro será exibido nos cinemas",
+        "content": "Prepare-se para o retorno mais aguardado do universo Marvel! 'Homem de Ferro: Renascimento' traz uma nova era de tecnologia, ação e surpresas, enquanto um novo herói assume a armadura lendária. Estreia em breve nos cinemas. Você está pronto para o futuro?"
     },
     {
-        "id":"2",
-        "photo":"aaa",
-        "title": "saiu novo filme",
-        "description":"ola"
+        "Id": "2",
+        "photo": "https://lumiere-a.akamaihd.net/v1/images/disney_default_pic_for_four_articles_d9b64713.png?region=40,0,1120,630&width=960",
+        "title": "Disney+ contará com nova série da Marvel para 2025",
+        "content": "Embarque em uma jornada épica com a nova série exclusiva da Disney+: Ecos do Multiverso. Personagens inéditos, realidades paralelas e uma trama que vai mexer com tudo o que você conhece do universo Marvel. Disponível em breve, só no Disney+."
     },
+    {
+        "Id": "3",
+        "photo": "https://sm.ign.com/ign_br/screenshot/default/x-men-3_w4tf.jpg",
+        "title": "Filme dos X-Men novo. Só lança filme de super herói pelo visto",
+        "content": "Os mutantes estão de volta em uma nova aventura cheia de tensão, poderes descontrolados e decisões que mudarão tudo. X-Men: Gênese Sombria mergulha nos segredos do passado e na luta pelo futuro. Estreia nos cinemas muito em breve."
+    },
+    {
+        "Id": "4",
+        "photo": "https://ogimg.infoglobo.com.br/in/24577295-2fe-e10/FT1500A/690/pantera-negra.jpg",
+        "title": "Filme do pantera negra vai ser o pipoco",
+        "content": "Wakanda ressurge com ainda mais força em Pantera Negra: Legado de Wakanda. Uma nova liderança, antigos mistérios e a proteção do legado de T’Challa em uma história que promete emocionar e surpreender. Não perca essa nova fase!"
+    }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { dataFake } from '../../components/data/dataFake';
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent implements OnInit {
-  
-  photoCover:string = "https://recreio.com.br/media/_versions/marvel/tony_stark_capa_widelg.jpg"
-  contentTitle:string ="TITULO"
-  contentDescription:string="Ola mundo"
-  private id:string = "0"
+
+  private id: string | null = "0";
+
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+    )
+    console.log(this.id);
 
+    this.setValuesToComponent(this.id);
   }
 
-  constructor(private route:ActivatedRoute){
-    
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.Id == id)[0];
+
+    this.photoCover = result.photo;
+    this.contentTitle = result.title;
+    this.contentDescription = result.content;
   }
-  
 }
